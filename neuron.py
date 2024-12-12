e5eb1d375f1e7f7d5274917b16b588c9fbc6d484
Oluwatobi Olajide <tobiolajide01@gmail.com>
2024-12-12 03:26:47 +0000

    created a neural network to implement feed forward

--- neuron.py
+++ neuron.py
@@ -11,3 +11,25 @@ class Neuron:
         total = np.dot(self.weights, inputs) + self.bias
         return sigmoid(total)
 
+
+class NeuralNetwork:
+    def __init__(self):
+        weights = np.array([0,1])
+        bias = 0
+
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+
+    def feedforward(self, x):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h1.feedforward(x)
+
+        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
+        return out_o1
+
+
+nn = NeuralNetwork()
+inputs =np.array([2, 3])
+out = nn.feedforward(inputs)
+print(out)
